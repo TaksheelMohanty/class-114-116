72ae85e4c79357854e937cf067ae2bd6be644288
TaksheelMohanty <72607083+TaksheelMohanty@users.noreply.github.com>
2020-11-27 17:45:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-function preload(){
+mustacheX = 0;
+MustacheY = 0;
 
+function preload(){
+    mustache = loadImage('https://i.postimg.cc/vHghkJyG/mustache.png');
 }
 
 function setup(){
-    canvas = createCanvas(1600, 900);
-    canvas.position (150, 150);
-    video = createCapture(VIDEO)
+    canvas = createCanvas(350, 350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350,350);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('poseNet is established');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        mustacheX = results[0].pose.nose.x - 15;
+        MustacheY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
 
 function draw(){
-    image(video, 600, 300, 400,300);
-    
-    fill('rgb(255,0,0)');
-    circle(1500, 800, 100);
-    
-    fill('rgb(255,0,0)');
-    circle(100, 100, 100);
-    
-    fill('rgb(255,0,0)');
-    circle(1500, 100, 100);
-    
-    fill('rgb(255,0,0)');
-    circle(100, 800, 100);
-    
-    fill('rgb(34,139,34)');
-    rect(150, 785, 1300, 30);
-    
-    fill('rgb(34,139,34)');
-    rect(150, 85, 1300, 30);
-    
-    fill('rgb(34,139,34)');
-    rect(1485, 150, 30, 600);
+    image(video, 0, 0, 350, 350);
+    image(mustache, mustacheX, MustacheY, 30, 30);
 
-    fill('rgb(34,139,34)');
-    rect(85,150, 30, 600)
-    
 }
 
 function take_snapshot(){
-    save('your_image.png');
+    save('myfilter.png');
 }
+
